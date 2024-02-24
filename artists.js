00f212fd167d872b1a3c0ca9051c883c5ec83dff
khushisingh2911 <115849049+khushisingh2911@users.noreply.github.com>
2024-02-23 23:13:43 -0500

    Create artists.js

---
+++ artists.js
@@ -0,0 +1,42 @@
+// Define an array of artists with unique identifiers, names, and social media links
+window.artists = [
+  {
+    // Unique identifier for the artist
+    artistId: "AID-1",
+    // Name of the artist
+    name: "Diljit Dosanjh",
+    // Array of social media links associated with the artist
+    urls: [
+      // Instagram link for Diljit Dosanjh
+      { url: "https://www.instagram.com/diljitdosanjh/", name: "Instagram" },
+      // Twitter link for Diljit Dosanjh
+      { url: "https://twitter.com/diljitdosanjh?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor", name: "Twitter" },
+    ],
+  },
+  {
+    // Unique identifier for the artist
+    artistId: "AID-2",
+    // Name of the artist
+    name: "Karan Aujla",
+    // Array of social media links associated with the artist
+    urls: [
+      // Instagram link for Karan Aujla
+      { url: "https://www.instagram.com/karanaujla_official/", name: "Instagram" },
+      // Twitter link for Karan Aujla
+      { url: "https://twitter.com/GeetanDiMachine", name: "Twitter" },
+    ],
+  },
+  {
+    // Unique identifier for the artist
+    artistId: "AID-3",
+    // Name of the artist
+    name: "Ap Dhillion",
+    // Array of social media links associated with the artist
+    urls: [
+      // Instagram link for Ap Dhillion
+      { url: "https://www.instagram.com/ap.dhillxn/?hl=en", name: "Instagram" },
+      // Twitter link for Ap Dhillion
+      { url: "https://twitter.com/apdhillxn/status/1669800913022877697", name: "Twitter" },
+    ],
+  }
+];
